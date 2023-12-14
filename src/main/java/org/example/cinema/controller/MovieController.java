1df17e177c991dac90da19db985afe37bf5bba30
hed <houssemeddine.dhouib@alliance4u.fr>
2023-12-14 10:36:17 +0100

    add movie controller

---
+++ src/main/java/org/example/cinema/controller/MovieController.java
@@ -0,0 +1,15 @@
+package org.example.cinema.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/movie")
+public class MovieController {
+
+    @GetMapping("")
+    public String info(){
+        return "api movie";
+    }
+}
